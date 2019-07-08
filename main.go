9a6e002bd3be75f58ad3b307e1d15b75a37352fc
李括 <likuo@ifeng.com>
2019-07-08 16:57:40 +0800

    fixed: error msg return method of the main

--- main.go
+++ main.go
@@ -11,11 +11,13 @@ import (
 func main() {
 	basePath, err := path.GetProjectPath(3)
 	if err != nil {
-		fmt.Sprintln(err)
+		fmt.Println(err)
+		return
 	}
 	cfg, err := config.ConfigParse(basePath)
 	if err != nil {
-		fmt.Sprintln(err)
+		fmt.Println(err)
+		return
 	}
 	r, _ := client.NewClient(cfg.Registry.Property, cfg.KVCache.CacheObj, cfg.KVCache.WCacheObj, cfg.KVCache.ModuleName)
 	cmd.Execute(r)
